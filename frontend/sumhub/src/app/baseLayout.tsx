eecf6fd6e813ef32cde151d4e688cb10e53b1c82
AhmadNajiKam <ahmadnajikam@gmail.com>
2024-11-09 21:56:59 +0300

    Fix some code

--- frontend/sumhub/src/app/baseLayout.tsx
+++ frontend/sumhub/src/app/baseLayout.tsx
@@ -1,23 +1,28 @@
 import { LogoHolder } from "../components/ui/LandingPage";
 import { Button } from "../components/ui/button";
+// Define the prop types for DefaultLayout
+
+interface DefaultLayoutProps {
+  children: React.ReactNode;
+  onClickfun: () => void; // Type for onClick handler
+}
 
 export default function DefaultLayout({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+  onClickfun, // Destructure the props
+}: DefaultLayoutProps) {
   return (
-    
     <div className="min-h-screen flex flex-col">
-      <div className=" p-8 flex justify-between items-center">
-        <LogoHolder></LogoHolder>
+      <div className="p-8 flex justify-between items-center">
+        <LogoHolder />
         <div>
-          <Button variant="ghost">Login</Button>
-          <Button className="ml-2">Sign Up</Button>{" "}
+          <Button variant="ghost" onClick={onClickfun}>
+            Login
+          </Button>
+          <Button className="ml-2">Sign Up</Button>
         </div>
       </div>
       {children}
     </div>
-    
   );
 }
--- frontend/sumhub/src/pages/index.tsx
+++ frontend/sumhub/src/pages/index.tsx
@@ -9,18 +9,18 @@ import { Input } from "@/components/ui/input";
 export default function Home() {
   const router = useRouter();
 
-  // const handleLogin = () => {
-  //   // Navigate to the login page
-  //   router.push("/login"); // Replace '/login' with your desired path
-  // };
+   const handleLogin = () => {
 
-  // const handleSignUp = () => {
-  //   // Navigate to the sign-up page
-  //   router.push("/signup"); // Replace '/signup' with your desired path
-  // };
+    router.push("/login"); 
+  };
+/*
+  const handleSignUp = () => {
 
+  router.push("/signup");
+   };
+*/
   return (
-    <DefaultLayout>
+    <DefaultLayout onClickfun={handleLogin}>
       <div className="flex-grow flex flex-col items-center justify-center px-4">
         <div className="w-full max-w-md p-9 space-y-4 text-center">
           <LandingText />
